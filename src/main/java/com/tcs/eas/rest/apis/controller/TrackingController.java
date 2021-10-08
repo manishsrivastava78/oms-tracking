8c17aa28431477f357058cbc54f1d027e568dbe8
manishsrivastava78 <manishsrivastava78@gmail.com>
2021-10-08 17:29:24 +0530

    add cors

--- src/main/java/com/tcs/eas/rest/apis/controller/TrackingController.java
+++ src/main/java/com/tcs/eas/rest/apis/controller/TrackingController.java
@@ -11,6 +11,7 @@ import javax.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,7 @@ import com.tcs.eas.rest.apis.utility.Utility;
  */
 @RestController
 @RequestMapping("/apis/v1")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 public class TrackingController {
 
 	@Autowired
